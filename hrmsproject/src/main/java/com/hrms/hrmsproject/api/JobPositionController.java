702e2e6e8a5c574931c0372ba8eb4f35915c4e8b
Soner Çelik <snrclk94@outlook.com>
2021-07-15 14:16:37 +0300

    .

--- hrmsproject/src/main/java/com/hrms/hrmsproject/api/JobPositionController.java
+++ hrmsproject/src/main/java/com/hrms/hrmsproject/api/JobPositionController.java
@@ -2,6 +2,7 @@ package com.hrms.hrmsproject.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class JobPositionController {
 	
 	private JobPositionService jobPositionService;
 
+	@Autowired
 	public JobPositionController(JobPositionService jobPositionService) {
 		this.jobPositionService = jobPositionService;
 	}
